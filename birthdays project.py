29a109b9a35de255f497382afaf6671bd6df9f36
laxmiteja-12345 <laxmitejaadlapalli959@gmail.com>
2026-05-07 11:32:34 +0530

    Add files via upload

---
+++ birthdays project.py
@@ -0,0 +1,15 @@
+Rakesh = input("Enter your birthday Rakesh: ")
+print("Rakesh's birthday: ",Rakesh)
+
+Harsh = input("Enter your birthday Harsh: ")
+print("Harsh's birthday: ",Harsh)
+
+Aarav = input("Enter your birthday Aarav: ")
+print("Aarav's birthday: ",Aarav)
+
+Arjun = input("Enter your birthday Arjun: ")
+print("Arjun's birthday: ",Arjun)
+
+Karthik = input("Enter your birthday Karthik: ")
+print("Karthik's birthday: ",Karthik)
+
